4e1c97a1866135290b79c27a73685a3a709c2e6a
Aavtic <aadishm79@gmail.com>
2024-09-29 00:23:48 +0530

    Added Backend. Slow for now.

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,5 @@ use web_server::axum_serve;
 
 
 pub fn server() {
-    axum_serve::code_output_api("0.0.0.0:8080", "/api/v1");
+    axum_serve::code_output_api("127.0.0.1:8081", "/api/v1");
 }
--- src/web_server/axum_serve.rs
+++ src/web_server/axum_serve.rs
@@ -1,4 +1,4 @@
-use axum::{Router, routing::post, extract::Json};
+use axum::{Router, routing::{post,options}, extract::Json, response::Response, http::StatusCode, body::Body};
 use serde::{Serialize, Deserialize};
 //use serde_json::to_string_pretty;
 use crate::runner::{
@@ -14,19 +14,39 @@ struct CodeRequest {
 }
 
 
-async fn get_code_output(Json(json_request): Json<CodeRequest>) -> String {
+async fn get_code_output(Json(json_request): Json<CodeRequest>) -> Response {
     let code = json_request.code;
     std::fs::write("./code/python-code/code.py", code.as_bytes()).expect("ERROR WRITING TO FILE.");
     docker_build_python::docker_build();
     let code_output = docker_python_execution::run_python_code();
 
-    return code_output;
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header("Access-Control-Allow-Origin","*")
+        .header("Access-Control-Allow-Methods", "POST")
+        .header("Access-Control-Allow-Headers","Content-Type")
+        .body(Body::from(code_output))
+        .unwrap();
+    return response;
 } 
 
 
+async fn preflight_response() -> Response {
+    let response = Response::builder()
+        .status(StatusCode::NO_CONTENT)
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+        .header("Access-Control-Allow-Headers", "Content-Type")
+        .body(Body::default())
+        .unwrap();
+    return response;
+}
+
 #[tokio::main]
 pub async fn code_output_api(addr: &str, path: &str) { 
-    let app = Router::new().route(path, post(get_code_output));
+    let app = Router::new()
+        .route(path, post(get_code_output))
+        .route(path, options(preflight_response));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
