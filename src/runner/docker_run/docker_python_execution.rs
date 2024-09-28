c885499adf14a3fda64262abff22b4083014dffa
Aavtic <aadishm79@gmail.com>
2024-09-28 19:16:37 +0530

    Docker Runner for python execution.

--- src/runner/docker_run/docker_python_execution.rs
+++ src/runner/docker_run/docker_python_execution.rs
@@ -1,10 +1,18 @@
 use std::process::Command;
 
 pub fn run_python_code() -> String {
-    let command = Command::new("docker")
-        .args(["run", "--rm", "secure-python"])
-        .output()
-        .expect("ERROR RUNNING COMMAND!");
-    return String::from_utf8(command.stdout).unwrap();
+    let command = Command::new("sudo")
+        .args(["docker", "run", "--rm", "secure-python"])
+        .output();
+    let output = match command {
+        Ok(output) => {
+            String::from_utf8(output.stdout).unwrap()
+        },
+        Err(e) => {
+            format!("{:?}", e)
+        }
+    };
+
+    return output;
 }
 
