daccd7e005200369fef319578a614ca8f164772d
Aavtic <aadishm79@gmail.com>
2024-10-23 20:55:05 +0530

    Python Server for Code output/

--- src/language_servers/python_ls/server.py
+++ src/language_servers/python_ls/server.py
@@ -1,8 +1,12 @@
 import socket
+import threading
 import os
+import json
+import subprocess
 
 
 IPC_SOCK = "./ipc.sock"
+RUNNER = "./runner.py"
 
 try:
     os.unlink(IPC_SOCK)
@@ -13,23 +17,44 @@ except OSError:
 
 class Server:
     def __init__(self):
-        pass
+        self.running = True
 
     def serve(self):
         self.s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.s.bind(IPC_SOCK)
         self.s.listen()
 
-        print("Server listening...")
-        conn, addr = self.s.accept()
-        print("Connection from ", addr)
+        while self.running:
+            print("Server listening...")
+            conn, addr = self.s.accept()
+            print("Connection from ", addr)
+            threading.Thread(target=self.handle_client, args=(conn,)).start()
 
+    def get_output(self, json_request):
+        code = json_request["code"]
+        proc = subprocess.Popen([
+            "python3",
+            RUNNER,
+            code
+            ], stdout=subprocess.PIPE)
+        out, err = proc.communicate()
+        print(out, err)
+
+    def handle_client(self, conn):
+        json_msg = ""
+        msg_buffer = ""
         try:
             while True:
-                msg = conn.recv(1024)
+                msg = conn.recv(1024).decode()
                 if msg:
-                    print("Msg: ", msg.decode())
-                    conn.sendall("Hello Client!".encode())
+                    msg_buffer += msg
+                    try:
+                        json_msg = json.loads(msg)
+                    except:
+                        continue
+                    print(json_msg)
+                    self.get_output(json_msg)
+                    msg_buffer = ""
                 else:
                     break
         finally:
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use coapi_rs;
+//use coapi_rs;
+mod language_client; 
 
 fn main() {
-    coapi_rs::server();
+    //coapi_rs::server();
+    language_client::python_client::python_client::get_output();
 }
