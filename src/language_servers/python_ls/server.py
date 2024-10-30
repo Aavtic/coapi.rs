303641cf7e9e4e5f2a1fe424f990d8df0af0911c
Aavtic <aadishm79@gmail.com>
2024-10-30 13:31:17 +0530

    (UPDATE): Updated server for accepting stdin from python client.

--- src/language_servers/python_ls/server.py
+++ src/language_servers/python_ls/server.py
@@ -2,7 +2,8 @@ import socket
 import threading
 import os
 import json
-import subprocess
+import queue
+import sys
 
 
 IPC_SOCK = "./ipc.sock"
@@ -30,36 +31,62 @@ class Server:
             print("Connection from ", addr)
             threading.Thread(target=self.handle_client, args=(conn,)).start()
 
-    def get_output(self, json_request):
-        code = json_request["code"]
-        proc = subprocess.Popen([
-            "python3",
-            RUNNER,
-            code
-            ], stdout=subprocess.PIPE)
-        out, err = proc.communicate()
-        print(out, err)
+    def get_output(self, json_request, que):
+        sin = Stdin(que)
+        sys.stdin = sin
+        print("Running...")
+        exec(json_request["code"])
 
     def handle_client(self, conn):
+        que = queue.Queue(maxsize=100)
+        get_userinput = False
         json_msg = ""
         msg_buffer = ""
         try:
             while True:
                 msg = conn.recv(1024).decode()
+                print("received", msg)
                 if msg:
                     msg_buffer += msg
                     try:
-                        json_msg = json.loads(msg)
-                    except:
+                        # print("trying: ", [i for i in msg_buffer])
+                        msg_buffer = msg_buffer.split("\x00")[0]
+                        print(msg_buffer)
+                        json_msg = json.loads(msg_buffer)
+                    except Exception as e:
+                        print(e)
                         continue
-                    print(json_msg)
-                    self.get_output(json_msg)
-                    msg_buffer = ""
+                    if not get_userinput:
+                        threading.Thread(target=self.get_output, args=(json_msg, que,)).start()
+                        msg_buffer = ""
+                        get_userinput = True
+                    else:
+                        print("got userinput", json_msg)
+                        que.put(json_msg)
+                        msg_buffer = ""
+
                 else:
                     break
         finally:
+            print("closing")
             conn.close()
             os.unlink(IPC_SOCK)
+
+
+class Stdin:
+    def __init__(self, que):
+        sys.stdin = self
+        self.que = que
+
+    def exec(self, code: str):
+        exec(code)
+        self.que.put("Done")
+
+    def readline(self):
+        data = self.que.get(block=True)
+        stdin_data = data["stdin"]
+
+        return stdin_data
 
 
 if __name__ == "__main__":
