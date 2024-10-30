465e9677c0bea01cade7ea660ef1c9e33ce4bf0f
Aavtic <aadishm79@gmail.com>
2024-10-30 13:30:43 +0530

    (FIX): Fixed Stdin issue.

--- src/language_client/python_client/python_client.rs
+++ src/language_client/python_client/python_client.rs
@@ -11,17 +11,45 @@ struct OutputRequest {
     code: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct StdinRequest{
+    stdin: String,
+}
+
+
+fn send_message(stream: &mut UnixStream, buffer: &mut Vec<u8>, message: &str) {
+    let message_bytes = message.as_bytes();
+
+    if message_bytes.len() > buffer.len() {
+        panic!("Message too big");
+    }
+
+    buffer.fill(0);
+    buffer[..message_bytes.len()].copy_from_slice(message_bytes);
+
+    stream.write_all(buffer).unwrap();
+}
 
 fn send_request(mut stream: UnixStream) {
-    let mut request = Vec::with_capacity(1024);
+    let mut request = vec![0u8; 1024];
     let mut response = [0; 1024];
     let json_request = OutputRequest {
-        code: "print('Hello World');\nprint(\"HELLO\")".to_string(),
+        code: "name = input('Enter your name');print('hello ', name);initial = input('Enter initial');print('full name:', name, initial);".to_string(),
+    };
+    let stdin_request1 = StdinRequest {
+        stdin: "Aadish".to_string(),
+    };
+    let stdin_request2 = StdinRequest {
+        stdin: "M".to_string(),
     };
     let json_msg = serde_json::to_string(&json_request).unwrap();
+    let stdin_msg1 = serde_json::to_string(&stdin_request1).unwrap();
+    let stdin_msg2 = serde_json::to_string(&stdin_request2).unwrap();
 
-    request.write_all(json_msg.as_bytes()).unwrap();
-    stream.write_all(&request).unwrap();
+    send_message(&mut stream, &mut request, &json_msg);
+    send_message(&mut stream, &mut request, &stdin_msg1);
+    send_message(&mut stream, &mut request, &stdin_msg2);
+
     stream.read(&mut response).unwrap();
     println!("{}", String::from_utf8(response.to_vec()).unwrap());
 }
