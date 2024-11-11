90e9a3f1bd77516c3c5aa666390966624dc35f4d
Aavtic <aadishm79@gmail.com>
2024-11-11 16:47:39 +0530

    Added JS, CSS for single question viewing.

--- coapi-frontend/scripts/view_questions.js
+++ coapi-frontend/scripts/view_questions.js
@@ -1,3 +1,15 @@
+
+function setUpOnQuestionClick() {
+    const divs = document.querySelectorAll('.question');
+
+    divs.forEach(div => {
+        div.addEventListener('click', function() {
+            window.open(`http://127.0.0.1:8081/pages/question/${this.id}`, '_blank');
+        });
+    });
+}
+
+
 function load_questions() {
     const request = new Request(
           "http://127.0.0.1:8081/api/v1/get_questions", {
@@ -17,14 +29,14 @@ function load_questions() {
         return response.text(); // Expecting an HTML string
     })
     .then(html => {
-        // Select the element where you want to inject the HTML
         const contentContainer = document.querySelector(".questions_container");
         contentContainer.innerHTML = html; // Inject the HTML into the DOM
+        setUpOnQuestionClick();
     })
     .catch(error => {
         console.error("Error loading content:", error);
     });
+
 }
 
 load_questions();
-
