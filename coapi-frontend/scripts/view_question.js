395b19cf6f5f6d09abf484942c9c2e96a9c3806c
Aavtic <aadishm79@gmail.com>
2024-11-27 07:42:15 +0530

    Some Frontend shit

---
+++ coapi-frontend/scripts/view_question.js
@@ -0,0 +1,28 @@
+const run_button = document.querySelector(".runbutton");
+const outputtext = document.querySelector(".outputext");
+
+function update_output(text) {  
+  outputtext.textContent = text;
+}
+
+run_button.onclick = () => {
+  const socket = new WebSocket("ws://127.0.0.1:8081/ws/get_live_output");
+
+
+  socket.addEventListener("open", (event) => {
+    const codeBox = document.querySelector(".codebox");
+
+    const myJson = {
+      "code": codeBox.value,
+      "language": "Python",
+    };
+    console.log("sending", JSON.stringify(myJson));
+    socket.send(JSON.stringify(myJson));
+  });
+
+  socket.addEventListener("message", (event) => {
+    let data = event.data
+    console.log("Message from server ", data);
+    update_output(data)
+  });
+}
