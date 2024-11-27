eb361115fa5c7b796bc0b84a40fc65762f54e61a
Aavtic <aadishm79@gmail.com>
2024-11-27 07:53:27 +0530

    Some frotend sh1t

--- coapi-frontend/scripts/view_question.js
+++ coapi-frontend/scripts/view_question.js
@@ -2,14 +2,15 @@ const run_button = document.querySelector(".runbutton");
 const outputtext = document.querySelector(".outputext");
 
 function update_output(text) {  
-  outputtext.textContent = text;
+    let curr_text = outputtext.textContent;
+    outputtext.textContent = curr_text + text;
 }
 
 run_button.onclick = () => {
   const socket = new WebSocket("ws://127.0.0.1:8081/ws/get_live_output");
 
 
-  socket.addEventListener("open", (event) => {
+  socket.addEventListener("open", (_event) => {
     const codeBox = document.querySelector(".codebox");
 
     const myJson = {
