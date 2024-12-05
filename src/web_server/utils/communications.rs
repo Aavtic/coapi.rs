9c7bd85f24631192e21c04ee972a1b46319ef09f
Aavtic <aadishm79@gmail.com>
2024-12-05 23:55:36 +0530

    Trait for converting string to buffers of Response.

---
+++ src/web_server/utils/communications.rs
@@ -0,0 +1,30 @@
+pub trait WSBuffer {
+     fn to_ws_buffers(&self, bufsize: usize) -> Vec<String>;
+ }
+
+impl WSBuffer for String {
+    fn to_ws_buffers(&self, bufsize: usize) -> Vec<String> {
+        let mut buffer = vec![char::default(); bufsize];
+        let mut res = vec![];
+
+        if self.len() < bufsize {
+            for (i, item) in self.chars().enumerate() {
+                buffer[i] = item;
+            }
+            res.push(buffer.iter().collect::<String>().clone());
+        } else {
+            for i in 0..self.len().div_ceil(bufsize) {
+                let start = bufsize * i;
+                let char_iter = self.chars().skip(start).take(bufsize);
+                for (i, item) in char_iter.enumerate() {
+                    buffer[i] = item;
+                }
+                res.push(buffer.iter().collect::<String>().clone());
+                buffer.fill(char::default());
+            }
+        }
+
+        return res;
+    }
+}
+
