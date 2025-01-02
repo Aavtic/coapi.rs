cc32b08a518dd43e8c5762aa1ede9c7f13b437ff
Aavtic <aadishm79@gmail.com>
2025-01-03 00:35:22 +0530

    Start Python process to generate template.

--- src/web_server/utils/generate_python_binding.rs
+++ src/web_server/utils/generate_python_binding.rs
@@ -1,5 +1,6 @@
 use crate::axum_serve::ExpectedInputOutput;
 use std::process::Command;
+use std::io::ErrorKind;
 use serde::{Serialize, Deserialize};
 use serde_json;
 
@@ -20,20 +21,31 @@ use serde_json;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GenInput {
-    title: String,
-    description: String,
-    question_id: String,
-    function_name: String,
-    argument_name: String,
-    input_output: Vec<ExpectedInputOutput>
+    pub title: String,
+    pub description: String,
+    pub question_id: String,
+    pub function_name: String,
+    pub argument_name: String,
+    pub input_type: String,
+    pub output_type: String,
+    pub input_output: Vec<ExpectedInputOutput>
 }
 
-pub fn bind_gen_python(gen_input: GenInput) {
+pub fn bind_gen_python(gen_input: GenInput) -> Result<String, std::io::ErrorKind> {
     let input_json = serde_json::to_string(&gen_input).unwrap();
+    println!("{}", input_json);
     let command = Command::new("python3")
-        .arg("/test_code/generator_python/generate.py")
+        .arg("./test_code/generator_python/generate.py")
         .arg("--details")
-        .arg(input_json)
+        .arg(input_json + "\n")
         .output()
         .unwrap();
+    let output = String::from_utf8(command.stdout).unwrap();
+    let output_err = String::from_utf8(command.stderr).unwrap();
+    println!("Output: {}, {}", output, output_err);
+    if command.status.success() {
+        return Ok(output);
+    } else {
+        return Err(ErrorKind::NotFound);
+    }
 }
