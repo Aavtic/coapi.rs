0ffd021b090cfec16f3779bfafb5b3edb2553883
Aavtic <aadishm79@gmail.com>
2025-01-05 03:25:15 +0530

    Updated UI

--- coapi-frontend/scripts/coide.js
+++ coapi-frontend/scripts/coide.js
@@ -5,6 +5,8 @@ const inneroutput = document.getElementById("inneroutput");
 const outputext = document.querySelector(".outputext");
 const cursor = document.querySelector(".cursor");
 const statusbar = document.querySelector(".statustext");
+const output_text = document.querySelector(".output-text");
+const sub_text = document.querySelector(".sub-text");
 
 
 console.log("coide");
@@ -13,7 +15,14 @@ let msg_buffer = [];
 let keyboardListener;
 
 
+function clear_msgs() {
+    statusbar.textContent = "";
+    output_text.textContent = "";
+    sub_text.textContent = "";
 
+    output_text.id = "nil";
+    sub_text.id = "nil";
+}
 
 function update_output(jsonResp) {  
     cursor.remove();
@@ -93,13 +102,45 @@ function run_button_test() {
     fetch(request)
       .then((response) => {
           if (response.status === 200) {
-              return response.text;
+              return response.text();
           } else {
               throw new Error("API request failed!");
           }
       })
     .then((text) => {
-        console.log(text);
+        console.log("got response: " + text);
+        let test_results = JSON.parse(text);
+        console.log(test_results);
+        let status = test_results["status"];
+        clear_msgs();
+        
+        //Cooked,
+        //URCodeErrorLOL,
+        //URCodeDontReturnAnything,
+        if (status === "Pass") {
+            output_text.textContent = "Passed all test cases.";
+            output_text.id = "pass_id";
+        } else if (status["Fail"] != undefined) {
+            let expected = status["Fail"]["ex"];
+            let got = status["Fail"]["got"];
+            let input = status["Fail"]["input"];
+            output_text.textContent = "Test Case Failed";
+            sub_text.textContent = `Input: ${input}, Expected :${expected} but got: ${got}.`
+            sub_text.id = "fail_id_sub"
+            output_text.id = "fail_id";
+        }else if (status["URCodeErrorLOL"] != undefined) {
+            let error = status["URCodeErrorLOL"]["error"];
+            output_text.textContent = "Error:";
+            sub_text.textContent = `Error: ${error}`
+            output_text.id = "error_id";
+            sub_text.id = "error_id_sub";
+        } else if (status === "URCodeDontReturnAnything") {
+            output_text.textContent = "Your code may not return values in all cases";
+            output_text.id = "noreturn_id";
+        } else if (status === "Cooked") {
+            output_text.textContent = "SERVER FAULT please retry";
+            output_text.id = "cooked_id";
+        }
     })
     .catch((error) => {
         console.error(error);
