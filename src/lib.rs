d12408c7610ceec3f5d397bed24d9b70d90bc616
Aavtic <aadishmkerala@gmail.com>
2025-03-03 07:22:16 +0530

    Updates

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,9 @@ pub mod poller;
 
 use web_server::axum_serve;
 
-pub const WEB_HOST: &str = "127.0.0.1";
+pub const WEB_HOST: &str = "192.168.213.165";
+pub const WEB_PORT: i32 = 8081;
 
 pub fn server() {
-    axum_serve::code_output_api(&format!("{}:8081", WEB_HOST));
+    axum_serve::code_output_api(&format!("{}:{}", WEB_HOST, WEB_PORT));
 }
